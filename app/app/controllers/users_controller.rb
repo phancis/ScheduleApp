24f0736723cc870ba5ab3cf8b3eaea5d69f72d9c
Francis Phan <fphan@umass.edu>
2016-03-28 16:10:45 -0400

    Swaggy

---
+++ app/app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def edit
+  end
+end
